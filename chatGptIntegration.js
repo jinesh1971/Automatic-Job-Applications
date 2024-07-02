6532716d25208b0ba9a4cbec77fe7ea33176f1d5
Kajal Gawri <kajalgawri@Kajals-Laptop.local>
2024-07-02 17:17:58 -0400

    new changes

--- chatGptIntegration.js
+++ chatGptIntegration.js
@@ -7,15 +7,15 @@ const userProfile = JSON.parse(fs.readFileSync('userProfile.json', 'utf8'));
 
 // Initialize OpenAI client with your API key
 const openai = new OpenAI({
-    apiKey: process.env.API_KEY,  // Replace with your actual API key
-    organization: process.env.ORG_KEY,   // Replace with your organization ID
+    apiKey: process.env.API_KEY,        // Replace with your actual API key
+    organization: process.env.ORG_KEY,  // Replace with your organization ID
     project: process.env.PROJ_KEY       // Replace with your project ID
 });
 
 // Function to fetch a chat completion
 async function getChatCompletion(q) {
     // const q = 'What is the experience?'
-    const question = `Given the following user profile, answer the question appropriately (If a number such as years is asked, only provide a number please). \n\nUser Profile: ${JSON.stringify(userProfile)}\n\nQuestion: ${q}\n\nAnswer:`;
+    const question = `Given the following user profile, answer the question appropriately (If a number such as years is asked, only provide a number please). If unable to determine the answer from the profile, then if number is asked, return 0. if text is asked, return no such experience \n\nUser Profile: ${JSON.stringify(userProfile)}\n\nQuestion: ${q}\n\nAnswer:`;
     try {
         const completion = await openai.chat.completions.create({
             model: 'gpt-4o',
